1ae845446afb9b8ceb74fa89348b341cf77dab26
Toheed Khan <consultant-toheed.khan@mcb.mu>
2025-04-25 23:31:18 +0530

    Added logic to display wishes

--- Wishlist/Wishlist/ContentView.swift
+++ Wishlist/Wishlist/ContentView.swift
@@ -11,14 +11,16 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     
+    @Query private var wishes: [Wish]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            List {
+                ForEach(wishes) { wish in
+                    Text(wish.title)
+                }
+            } //: LIST
+        }.navigationTitle("Wishlist")
     }
 }
 
