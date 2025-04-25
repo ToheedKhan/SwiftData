c80991565bb5b058bbaad3d735526d9b031fa75a
Toheed Khan <consultant-toheed.khan@mcb.mu>
2025-04-25 11:55:14 +0530

    Added model container and model context

--- Wishlist/Wishlist/ContentView.swift
+++ Wishlist/Wishlist/ContentView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
