f0b80850be5fbbf9c75c29876ff1f9a95682ebc2
Toheed Khan <consultant-toheed.khan@mcb.mu>
2025-04-28 11:40:38 +0530

    Added overlay and logic to preview sample data

--- Wishlist/Wishlist/ContentView.swift
+++ Wishlist/Wishlist/ContentView.swift
@@ -19,14 +19,36 @@ struct ContentView: View {
             List {
                 ForEach(wishes) { wish in
                     Text(wish.title)
+                        .font(.title.weight(.light))
+                        .padding(.vertical, 2)
                 }
             } //: LIST
             .navigationTitle("Wishlist")
+            .overlay {
+                if wishes.isEmpty {
+                    ContentUnavailableView("My Wishlist",systemImage: "heart.circle", description: Text("No wishes yet. Add one to get started"))
+                }
+            }
         }
     }
 }
 
-#Preview {
+#Preview("List with Sample Data") {
+    let container = try! ModelContainer(for: Wish.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+    container.mainContext.insert(Wish(title: "Master SwiftData"))
+    container.mainContext.insert(Wish(title: "Travel to Europe"))
+
+    container.mainContext.insert(Wish(title: "Make a positive impact"))
+
+    container.mainContext.insert(Wish(title: "Buy a new iPhone"))
+
+    container.mainContext.insert(Wish(title: "Practive DSA"))
+    
+    return ContentView().modelContainer(container)
+        
+}
+
+#Preview("Empty List") {
     ContentView()
         .modelContainer(for: Wish.self, inMemory: true)
 }
