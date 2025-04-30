aa8707e8f9f2ad12754f94b7ed4710a3e77469d4
Toheed Khan <consultant-toheed.khan@mcb.mu>
2025-04-30 14:08:06 +0530

    Added Pet Sample data and logic to show data on UI

--- Paws/Paws/Paws/ContentView.swift
+++ Paws/Paws/Paws/ContentView.swift
@@ -12,17 +12,62 @@ struct ContentView: View {
     @Environment(\.modelContext) var  modelcontect
     @Query private var pets: [Pet]
     
+    let layout = [
+        GridItem(.flexible(minimum: 120)),
+        GridItem(.flexible(minimum: 120))
+    ]
+    
     var body: some View {
         NavigationStack {
             ScrollView {
-               EmptyView()
+                LazyVGrid(columns: layout) {
+                    GridRow {
+                        ForEach(pets) {
+                            pet in
+                            NavigationLink(destination: EmptyView()) {
+                                VStack {
+                                    if let imageData = pet.photo {
+                                        if let image = UIImage(data: imageData) {
+                                            Image(uiImage: image)
+                                                .resizable()
+                                                .scaledToFit()
+                                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
+                                        }
+                                    } else {
+                                        Image(systemName:  "pawprint.circle")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .padding(40)
+                                            .foregroundStyle(.quaternary)
+                                    }
+                                    Spacer()
+                                    Text(pet.name).font(.title.weight(.light))
+                                        .padding(.vertical)
+                                    Spacer()
+                                } //: VSTACK
+                                .frame(minWidth: 0,  maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                                .background(.ultraThinMaterial)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
+                            } //: NAVLINK
+                            .foregroundStyle(.primary)
+                        } //: LOOP
+                    } //: GRID ROW
+                }//: GRID LAYOUT
+                .padding(.horizontal)
             } //: SCROLL VIEW
+            .navigationTitle(pets.isEmpty ? "" : "Paws")
             .overlay {
                 if pets.isEmpty {
                     CustomContentUnavailableView(icon: "dog.circle", title: "No Pets", description: "Add a new pet to get started.")
                 }
-            }        } //: NAVSTACK
-    }
+            }
+        } //: NAVSTACK
+    }//: BODY
+}
+
+
+#Preview("Sample Data") {
+    ContentView().modelContainer(Pet.preview)
 }
 
 #Preview("No Data") {
