475d0a2fde07768236bd24da58345a31721ba613
Toheed Khan <consultant-toheed.khan@mcb.mu>
2025-05-01 16:28:36 +0530

    Added top leading slider and Implemented delete action

--- Paws/Paws/Paws/ContentView.swift
+++ Paws/Paws/Paws/ContentView.swift
@@ -14,12 +14,16 @@ struct ContentView: View {
     
     @State private var path = [Pet]()
     
+    @State private var isEditing: Bool = false
+    
     let layout = [
         GridItem(.flexible(minimum: 120)),
         GridItem(.flexible(minimum: 120))
     ]
     
     func addPet() {
+        isEditing = false
+        
         let pet = Pet(name: "Best Friend")
         modelContext.insert(pet)
         path = [pet]
@@ -57,6 +61,26 @@ struct ContentView: View {
                                 .frame(minWidth: 0,  maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
                                 .background(.ultraThinMaterial)
                                 .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
+                                .overlay(alignment: .topTrailing) {
+                                    if isEditing {
+                                        Menu {
+                                            Button("Delete", systemImage: "trash", role: .destructive) {
+                                              withAnimation {
+                                                modelContext.delete(pet)
+                                                try? modelContext.save()
+                                              }
+                                            }
+                                        } label: {
+                                            Image(systemName: "trash.circle.fill")
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fill)
+                                                .frame(width: 36, height: 36)
+                                                .foregroundStyle(.red)
+                                                .symbolRenderingMode(.multicolor)
+                                                .padding()
+                                        }
+                                    }
+                                }
                             } //: NAVLINK
                             .foregroundStyle(.primary)
                         } //: LOOP
@@ -67,6 +91,15 @@ struct ContentView: View {
             .navigationTitle(pets.isEmpty ? "" : "Paws")
             .navigationDestination(for: Pet.self, destination: EditPetView.init)
             .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        withAnimation {
+                            isEditing.toggle()
+                        }
+                    } label: {
+                        Image(systemName: "slider.horizontal.3")
+                    }
+                }
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Add a New Pet", systemImage: "plus.circle", action: addPet)
                 }
